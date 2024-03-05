9afe3b216b40848caf499880d31336259fd18f98
Daniel-Alencar <danielalencar746@gmail.com>
2024-03-05 18:26:04 -0300

    A

--- Placa ESP32 WROOM/Arduino IDE/Teste dos componentes/Módulo LoRa/code5/Envio/Envio.ino
+++ Placa ESP32 WROOM/Arduino IDE/Teste dos componentes/Módulo LoRa/code5/Envio/Envio.ino
@@ -2,35 +2,27 @@
 Os pinos M0 e M1 do módulo LoRa foram conectados diretamente no GND
 */
 
-#define BTN1 4
-#define BTN2 5
+#define BTN1  4
+#define BTN2  5
 
-#define M0 35
-#define M1 34
- 
-String turnOn = "on";
-String turnOff = "off";
+String turnOn = "00000,00000,00000,00000,00000,00000,00000,00000,00000,0000";
+String turnOff = "10000,00000,00000,00000,00000,00000,00000,00000,00000,0000";
 
-HardwareSerial LoRaSerial(2);
+HardwareSerial loraSerial(2);
 
 void setup() {
   pinMode(BTN1, INPUT_PULLUP);
   pinMode(BTN2, INPUT_PULLUP);
   Serial.begin(9600);
-
-  pinMode(M0, INPUT);
-  pinMode(M1, INPUT);
-
-  LoRaSerial.begin(9600, SERIAL_8N1, 16, 17);
+  loraSerial.begin(9600, SERIAL_8N1, 16, 17);
 }
  
 void loop() {
- 
+  loraSerial.print(turnOn);
   Serial.println(turnOn);
-  LoRaSerial.println(turnOn);
-  delay(500);
+  delay(1250);
 
+  loraSerial.print(turnOff);
   Serial.println(turnOff);
-  LoRaSerial.println(turnOff);
-  delay(500);
+  delay(1250);
 }
